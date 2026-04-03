448893c90080cbe77b5f4003b8ddf9db11954d77
Kalpesh Ingle <ingle.kalpesh93@gmail.com>
2026-04-03 08:44:30 +0000

    myfunction

--- srv/service.cds
+++ srv/service.cds
@@ -2,6 +2,11 @@ service product {
 
 function printhelloworld
 ( input : String ) returns String;
-    
+
+function addition( num : Integer, num2 : Integer) returns Integer;
+
+@open    
+type object{};    
+function Myfun(category: Integer ) returns object;
 
 }
--- srv/service.js
+++ srv/service.js
@@ -6,6 +6,41 @@ module.exports = cds.service.impl(async function(srv) {
     console.log(req.data.input)
     return `${req.data.input}world`
 
-    } )
-    
+} )
+
+   srv.on('addition', req =>{
+   console.log(req.data)
+   let result =  req.data.num + req.data.num2;
+    return result
+
+})
+  srv.on('Myfun', req => {
+
+    let result = {}
+
+    if ( req.data.category == 1) {
+     result.product = 'BMW',
+     result.price = '1000 INR',
+     result.location = 'Mumbai'
+
+    } else {
+   
+        result.product = 'Audi';
+        result.stock = 100,
+        result.priceArray = [{
+          "Price": 5689,
+          "Discount": "10%"
+
+
+        }]
+
+    }
+   console.log(result)
+    return result;
+
+
+  })
+
+
+
 })
